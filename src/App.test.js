1c071ae75941ef50636da7e53bbab7653cff2f93
harpalgupta <harpal.gupta@gmail.com>
2019-02-01 16:03:18 +0000

    jest re-seed db after tests to deal with api patching

--- src/App.test.js
+++ src/App.test.js
@@ -1,34 +1,61 @@
-import React from 'react';
-import App from './App';
-import * as api from './api'
+import React from "react";
+import App from "./App";
+import * as api from "./api";
+import { rejects } from "assert";
+const fs = require("fs");
+const path = require("path");
 
-describe('React Tests', () => {
-  it('renders without crashing', () => {
+//function to re-seed database/json
+const reSeedDatbase = () => {
+
+  fs.copyFileSync(
+    path.resolve(__dirname, "../data/originalDB.json"),
+    path.resolve(__dirname, "../data/db.json"),
+
+    err => {
+      if (err) console.log(err);
+    }
+  );
+};
+
+//after all tests re-seed database
+afterAll(() => {
+  reSeedDatbase();
+});
+
+describe("React Tests", () => {
+  it("renders without crashing", () => {});
+});
+
+describe("check Api calls", () => {
+  it("retrieves 9 cards from api fetch", async () => {
+    return api.fetchCards().then(data => {
+      expect(Array.isArray(data)).toBe(true);
+      expect(data).toHaveLength(9);
+      expect(data[0].is_liked).toBe(true);
+    });
+  });
+
+
+
+  it("retrieves 3 cards from api fetch when given range", async () => {
+    return api.fetchCards(2,5).then(data => {
+      expect(Array.isArray(data)).toBe(true);
+      expect(data).toHaveLength(3);
+    });
+  });
+
+
+
+
+  it("patch like", async () => {
+    const result = await api.likeCard(1, false);
+
+    expect(result.is_liked).toBe(false);
+  });
+
+
+
+
 
 });
-})
-
-
-describe('check Api calls', () => {
- it('retrieves 9 cards from api fetch', async ()=>{
-  return api.fetchCards().then(data=>{
-    expect(Array.isArray(data)).toBe(true);
-    expect(data).toHaveLength(9);
-  
-  })
-})
-
-it('patch like', async ()=>{
-  
-  const result = await  api.likeCard(1,false)
-    // console.log(data)
-    
-   return result
-  
-  })
-
-
-}) 
-
-
-
--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,22 @@
-import axios from "axios"
+import axios from "axios";
 const BASEURL = "http://localhost:3001/cards";
 
+export const fetchCards = async (start, end) => {
+  let url = "";
+  if (start && end) {
+    url = `${BASEURL}/?_start=${start}&_end=${end}`;
+  } else {
+    url = `${BASEURL}/`;
+  }
+  const { data } = await axios.get(url);
+  return data;
+};
 
-export const fetchCards =async ()=>{
-
-const {data}=await axios.get(BASEURL);
-return data;
-
-}
 
 
- export const  likeCard=async(id,is_liked)=>{
-  const url= `${BASEURL}/${id}`
-  const {data}=await axios.patch(url,{is_liked:is_liked});
-return data;
 
-}
-
+export const likeCard = async (id, is_liked) => {
+  const url = `${BASEURL}/${id}`;
+  const { data } = await axios.patch(url, { is_liked: is_liked });
+  return data;
+};
