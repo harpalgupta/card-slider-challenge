38404bf19fde64ff4a3b01e21b9c255b6be1a2f2
Harpal Gupta <harpal.gupta@gmail.com>
2019-02-15 09:35:44 +0000

    lint fixes

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,10 @@
-import axios from "axios";
-const BASEURL = "http://localhost:3001/cards";
+import axios from 'axios';
+
+const BASEURL = 'http://localhost:3001/cards';
 
 export const fetchCards = async (start, end) => {
-  let url = "";
-  if (start>=0 && end>=0) {
+  let url = '';
+  if (start >= 0 && end >= 0) {
     url = `${BASEURL}/?_start=${start}&_end=${end}`;
   } else {
     url = `${BASEURL}/`;
@@ -13,10 +14,8 @@ export const fetchCards = async (start, end) => {
 };
 
 
-
-
 export const likeCard = async (id, is_liked) => {
   const url = `${BASEURL}/${id}`;
-  const { data } = await axios.patch(url, { is_liked: is_liked });
+  const { data } = await axios.patch(url, { is_liked });
   return data;
 };
